2f8baa1101a3cfe5ebc791858dd98834b9f10dec
Russell Wallace <russell.wallace@gmail.com>
2024-04-21 09:10:46 +0100

    initial

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TestSystemDataSQLite
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
